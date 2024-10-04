6ec02b73a71d395392cd269baf33fe1ded48b0d6
Osama Sholi <117115944+osama-sholi@users.noreply.github.com>
2024-10-04 15:04:46 +0300

    Time: 0 ms (100.00%) | Memory: 9.7 MB (76.27%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.cpp
@@ -2,24 +2,17 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
         int seconds = 0;
-        while (true) {
-            tickets[0]--;
-            rotate(tickets.begin(), tickets.begin() + 1, tickets.end());
-
-            if (tickets[tickets.size() - 1] == 0) {
-                if (k == 0) {
-                    seconds++;
-                    return seconds;
-                }
-                tickets.pop_back();
+        int i = 0;
+        while(tickets[k]){
+            if(tickets[i] == 0){
+                i = (i + 1) % tickets.size();
+                continue;
             }
-            if (k == 0) {
-                k = tickets.size() - 1;
-            }
-            else {
-                k--;
-            }
+            tickets[i]--;
             seconds++;
+            i = (i + 1) % tickets.size();
         }
+
+        return seconds;
     }
 };
