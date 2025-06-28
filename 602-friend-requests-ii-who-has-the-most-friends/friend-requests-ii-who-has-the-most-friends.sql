9bc8c1f38e7bc7c8f167289392c264ebcad9651f
Osama Sholi <117115944+osama-sholi@users.noreply.github.com>
2025-06-28 21:21:27 +0300

    Time: 189 ms (37.12%) | Memory: 0B (100.00%) - LeetSync

--- 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
+++ 602-friend-requests-ii-who-has-the-most-friends/friend-requests-ii-who-has-the-most-friends.sql
@@ -1,13 +1,11 @@
 -- Write your PostgreSQL query statement below
-SELECT COALESCE(rc.id, ac.id) id, (COALESCE(rc.count, 0) + COALESCE(ac.count, 0)) num
+SELECT ra.id, COUNT(*) num
 FROM 
-(SELECT r.requester_id id, COUNT(*)
+(SELECT r.requester_id id
 FROM RequestAccepted r
-GROUP BY r.requester_id) rc
-FULL JOIN
-(SELECT r.accepter_id id, COUNT(*)
-FROM RequestAccepted r
-GROUP BY r.accepter_id) ac
-ON rc.id = ac.id
+UNION ALL
+SELECT r.accepter_id id
+FROM RequestAccepted r) ra
+GROUP BY ra.id
 ORDER BY num DESC
 LIMIT 1
